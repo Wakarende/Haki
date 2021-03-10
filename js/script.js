ec170be9eadfdcd5156db63b6376ccf4ff5cb778
Joy Kirii <joykirii@gmail.com>
2021-03-10 09:49:10 +0300

    add js to print appointment date and name of user

--- js/script.js
+++ js/script.js
@@ -1,5 +1,24 @@
 
-$(document).ready(function(event) {
-  event.preventDefault();
+  $(document).ready(function() {
+    $("#form-info").submit(function(event) {
+      event.preventDefault();
+    
+    $("#info-btn").click(function() {
+      var fullName = $("#fullname").val();
+      var dayOfApp = $("#day").val();
+      var monthOfApp = $("#month").val();
+      var yearOfApp = $("#year").val();
+      // var serviceType = $("#service option:selected").val();
 
-});
+      let date = new Date(yearOfApp + "/" + monthOfApp + "/" + dayOfApp);
+      
+
+      if (fullName == ""|| date == "" ||) {
+        alert("please fill in your information below")
+      }else {
+        $("#displayAppointment").text("Dear " + fullName + "," + "your appointment is on " + date + ".")
+        // $("#modeOfComm").text("Your consultation will be held via " + serviceType);
+      }
+    });
+    });
+  });
